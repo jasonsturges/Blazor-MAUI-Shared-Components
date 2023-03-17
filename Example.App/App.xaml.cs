30d5312da1982f66a1378a0377f63d0db426d696
Jason Sturges <jason@jasonsturges.com>
2023-03-16 22:34:10 -0500

    Add MAUI Blazor App

---
+++ Example.App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Example.App;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Example.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Example.App;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
